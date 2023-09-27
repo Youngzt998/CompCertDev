0eea4e7be1b3c2ae47ff4c2f3643cb53404e7b96
Jun Shirako <shirako@gatech.edu>
2023-09-27 16:06:42 -0400

    GEMM unroll-and-jam version (factor: i=2, j=2)

--- test/c/PolyBenchC-4.2.1/gcc-gemm-comp.sh
+++ test/c/PolyBenchC-4.2.1/gcc-gemm-comp.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo 'Compile base'
-gcc -O3 -I utilities -I linear-algebra/kernels/atax utilities/polybench.c linear-algebra/blas/gemm/gemm.c -o gemm.base
+gcc -O3 -I utilities -I linear-algebra/blas/gemm utilities/polybench.c linear-algebra/blas/gemm/gemm.c -o gemm.base
 
 echo 'Compile pluto'
-gcc -O3 -I utilities -I linear-algebra/kernels/atax utilities/polybench.c linear-algebra/blas/gemm/gemm.pluto.c -o gemm.pluto
+gcc -O3 -I utilities -I linear-algebra/blas/gemm utilities/polybench.c linear-algebra/blas/gemm/gemm.pluto.c -o gemm.pluto -lm
+
+echo 'Compile pluto unroll-and-jam (factor: i=2, j=2)'
+gcc -O3 -I utilities -I linear-algebra/blas/gemm utilities/polybench.c linear-algebra/blas/gemm/gemm.pluto.i2j2.c -o gemm.pluto.i2j2 -lm
